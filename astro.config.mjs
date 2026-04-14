b35dbe208bf83e25d0609932a706f8d7a70c8357
Keiji Lohier <keiji.lohier@gmail.com>
2026-04-14 13:23:06 -0400

    Move to cloudflare workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +14,10 @@ export default defineConfig({
   image: {
     responsiveStyles: true,
   },
+
   site: "https://www.keijilohier.com",
   integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
 });
