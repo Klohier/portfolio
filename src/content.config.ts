24b456e26c2e1d21c50c76e5d0cc2af3495ac670
Keiji Lohier <keiji.lohier@gmail.com>
2026-04-15 12:56:03 -0400

    fix image pipeline and edit layout for readability

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,36 +4,38 @@ import { glob } from "astro/loaders";
 
 const blogs = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/content/blogs" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    author: z.string(),
-    image: z
-      .object({
-        url: z.string(),
-        alt: z.string(),
-      })
-      .optional(),
-    tags: z.array(z.string()).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      author: z.string(),
+      image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      tags: z.array(z.string()).optional(),
+    }),
 });
 
 const projects = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/content/projects" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    author: z.string(),
-    image: z
-      .object({
-        url: z.string(),
-        alt: z.string(),
-      })
-      .optional(),
-    tags: z.array(z.string()).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      author: z.string(),
+      image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      tags: z.array(z.string()).optional(),
+    }),
 });
 
 export const collections = { blogs, projects };
